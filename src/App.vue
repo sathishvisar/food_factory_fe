f488806b1407135c7a9f1db8ff2b87499e4ac838
sathish-visar <sathish.visar@gmail.com>
2020-03-22 13:51:05 +0530

    init VUR app done.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/signin">Signin</router-link> |
+      <router-link to="/signup">Sigup</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Signin from './../views/Signin.vue'
+import Signup from './../views/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Signin',
+    name: 'Signin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signin.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
   }
 ]
 
