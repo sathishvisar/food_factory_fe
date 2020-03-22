1aadda1c1d2f888336cabddcb578634047dd4c4b
sathish-visar <sathish.visar@gmail.com>
2020-03-22 14:11:40 +0530

    signin & signup component add with vue material.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Signin from './../views/Signin.vue'
-import Signup from './../views/Signup.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
     path: '/Signin',
     name: 'Signin',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signin.vue')
+    component: () => import('../components/Signin.vue')
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+    component: () => import('../components/Signup.vue')
   }
 ]
 
